fc8658901f101aefe25e811f26f2cde59a6062df
trishaSolanki <141935315+trishaSolanki@users.noreply.github.com>
2023-12-03 23:17:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function stringChop(str, size) {
   // your code here
+	if (str === null) {
+    return [];
+  }
+
+  const result = [];
+  let index = 0;
+
+  while (index < str.length) {
+    result.push(str.slice(index, index + size));
+    index += size;
+  }
+
+  return result;
 }
+
 
 // Do not change the code below
 const str = prompt("Enter String.");
